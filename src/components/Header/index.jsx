39c1bcdfc506d45db3efd750cb5eb96e29f2b2b6
lucianosants <lucianola18@gmail.com>
2022-11-07 16:03:34 -0300

    challenge 1 add banner is done!

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,8 @@ import { StyledHeader } from './styles';
 export default function Header({ github, name, job }) {
 	return (
 		<StyledHeader>
-			{/* <img src='banner' /> */}
+			{/* <img className='user__banner' src='./banner.jpeg' /> */}
+			<div className='user__banner'></div>
 			<section className='user__info'>
 				<img src={`https://github.com/${github}.png`} />
 
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 export const StyledHeader = styled.header`
 	margin-top: 50px;
 
-	img {
-		width: 80px;
-		height: 80px;
-		border-radius: 50%;
-		border: 2px solid red;
+	.user__banner {
+		width: 100%;
+		height: 230px;
+		background-image: url('./banner.jpg');
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
 	}
 
 	.user__info {
@@ -16,5 +18,12 @@ export const StyledHeader = styled.header`
 		width: 100%;
 		gap: 16px;
 		padding: 16px 32px;
+
+		img {
+			width: 80px;
+			height: 80px;
+			border-radius: 50%;
+			border: 2px solid red;
+		}
 	}
 `;
